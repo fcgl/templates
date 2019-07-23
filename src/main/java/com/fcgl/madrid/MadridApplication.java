027799c9bc3de4b84df95752c730e49878b1293e
Jean Paul Torre <jean.paul.ruben@gmail.com>
2019-07-23 23:06:53 +0200

    java microservice. Add Springboot project with health check

---
+++ src/main/java/com/fcgl/madrid/MadridApplication.java
@@ -0,0 +1,13 @@
+package com.fcgl.madrid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MadridApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MadridApplication.class, args);
+	}
+
+}
